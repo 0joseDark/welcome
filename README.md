edf03cbd26cc1d670e6fdd8e883b87156d216f7b
0joseDark <josegrand65@gmail.com>
2024-09-22 16:13:07 +0100

    add
    a simple welcome web page, with server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # welcome
  a simple welcome web page, with server
+# pip install flask
+# http://127.0.0.1:5000/
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+# Criação da aplicação Flask
+app = Flask(__name__)
+
+# Rota para a página inicial
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Rota para a página 'Sobre'
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Rota para a página 'Contato'
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Executa o servidor Flask
+if __name__ == '__main__':
+    app.run(debug=True)
